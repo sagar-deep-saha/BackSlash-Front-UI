322ed94847e115853ee6592ceb600bc324cc448f
Sagar Deep Saha <sagarsaha@outlook.in>
2025-06-19 17:40:22 +0530

    url set updated for yupcha twitter

--- src/services/api.js
+++ src/services/api.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 const api = axios.create({
     baseURL: import.meta.DEV 
         ? 'http://localhost:8000' 
+        // : 'http://localhost:8000',
         : 'https://back-slash-back-server.vercel.app',
     headers: {
         'Content-Type': 'application/json',
